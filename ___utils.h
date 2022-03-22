b6ce0db0690e42debc1092f066766d65c6c7b803
ChaAnat <102150991+ChaAnat@users.noreply.github.com>
2022-03-22 22:09:29 +0200

    Create ___utils.h
    not related

---
+++ ___utils.h
@@ -0,0 +1,16 @@
+#include "globals.h"
+
+/* anat add data here? */
+int isWholeNum(char *valToCheck);
+
+/* anat add data here? */
+int isValidLbl (char *lblToCheck);
+
+/* anat add data here? */
+int isReserved (char *valToGet);
+
+/* anat add data here? */
+senteType getSenteTypeName (char *valToCheck);
+
+/* anat add data here? */
+void *tryToMalloc(long size);
