10e2c57686d558111374f7ee9f4631d5197d96df
ChaAnat <102150991+ChaAnat@users.noreply.github.com>
2022-03-22 22:11:40 +0200

    Update ___utils.h

--- ___utils.h
+++ ___utils.h
@@ -1,3 +1,13 @@
+/*
+ * utils.h
+ *
+ *  Created on: Mar 21, 2022
+ *      Author: user
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
 #include "globals.h"
 
 /* anat add data here? */
@@ -14,3 +24,6 @@ senteType getSenteTypeName (char *valToCheck);
 
 /* anat add data here? */
 void *tryToMalloc(long size);
+
+
+#endif /* UTILS_H_ */
