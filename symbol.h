fa0952f2801e892e4dceaa1fba5dc511f5a18049
ChaAnat <102150991+ChaAnat@users.noreply.github.com>
2022-03-23 15:16:23 +0200

    Update symbol.h

--- symbol.h
+++ symbol.h
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 
 #define SYMBOL_ASSIGN_SIGN ':'
-#define DATA_TYPE_DATA ".data"
-#define DATA_TYPE_STRING ".string"
+#define DATA_TYPE_DATA ".data" /* the params are array of whole number / numbers (negative and positive) */
+#define DATA_TYPE_STRING ".string" /* only one param and its legal string "abc" */
+#define DATA_TYPE_ENTRY ".entry" /* only one param and its the label name */
+#define DATA_TYPE_EXTERN ".extern" /* only one param and its the undefined label name  */
 
 char * get_symbol_name(char ** words);
 
