b3bf87d802a4d6e74bec4a914b1e611a6dc1fab3
ChaAnat <102150991+ChaAnat@users.noreply.github.com>
2022-03-23 15:13:50 +0200

    Update instructionsregister.h

--- instructionsregister.h
+++ instructionsregister.h
@@ -17,13 +17,38 @@ typedef enum addressingMode {
 	REGISTER = 3
 } AddressingMode;
 
+/* type of funct  */
+typedef enum funct {
+	/* relates to opcode 2 */
+	ADD_FUNCT = 10,
+	SUB_FUNCT = 11,
+
+	/* relates to opcode 5 */
+	CLR_FUNCT = 10,
+	NOT_FUNCT = 11,
+	INC_FUNCT = 12,
+	DEC_FUNCT = 13,
+
+	/* relates to opcode 9 */
+	JMP_FUNCT = 10,
+	BNE_FUNCT = 11,
+	JSR_FUNCT = 12,
+
+	/* NA anat */
+	NA_FUNCT = 0
+
+} funct;
+
 /* command struct */
 typedef struct {
 	unsigned id;
 	char * name;
+	funct * reqFunct;
 	AddressingMode * allowedAddressingsSrc;
 	AddressingMode * allowedAddressingsDest;
 }  Command;
+
+
 
 signed int get_command_id(char * command);
 
