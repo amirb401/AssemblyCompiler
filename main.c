6f59a15e1ce9cfa593b14ed126bd96954a0c1a2d
ChaAnat <102150991+ChaAnat@users.noreply.github.com>
2022-03-25 20:06:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include "fileprocess.h"
 
-int main(int argc, char *argv[])
+/* int main(int argc, char *argv[]) anat */
+int main()
 {
 
-	/* validate amount of files */
+	const char* fname = "/home/user/Desktop/new/tryyy/ps.as"; /* anat to remove */
+	FILE* fp = fopen(fname, "r"); /* anat to remove */
+
+	/* validate amount of files
 	if(argc < 2) {
 		printf("no file chosen...\n");
 		exit(0);
-	}
+	}*/
 
-	/*loop through the files*/
+	/*loop through the files
 	while(--argc) {
 		process_file(argv[argc]);
-	}
+	}*/
 
+	while (fp)
+	{
+		process_file(fname);
+	}
 	return 0;
 }
